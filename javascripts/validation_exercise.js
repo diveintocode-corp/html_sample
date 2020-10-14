f4910729d09ea9c49b6ef79a740aa90716b9be9b
YuukiIshibashi <robopowerful.v@gmail.com>
2020-10-14 13:27:05 +0900

    ajax追加

--- javascripts/validation_exercise.js
+++ javascripts/validation_exercise.js
@@ -1,18 +1,17 @@
 function emailValidation() {
   const form = document.getElementById('form');
-  const emailComfirmField = document.getElementById('email_confirm');
-  const contentField = document.getElementById('content_field');
-  console.log(contentField.parentNode)
-  const element = document.createElement('p')
-  const message = document.createTextNode("Eメールが一致しません")
-  element.classList.add("alert_color");
-  emailComfirmField.addEventListener('input', e => {
-    e.preventDefault();
-    if(form.email.value !== form.email_confirm.value) {
-      element.appendChild(message);
-      contentField.parentNode.insertBefore(element, contentField);
+  const emailComfirmField = document.getElementById('email_confirm'); // --2
+  const element = document.createElement('p'); // --4
+  const message = document.createTextNode("Eメールが一致しません"); // --5
+  element.appendChild(message); // --6
+  element.classList.add("alert_color"); // --7
+  emailComfirmField.addEventListener('input', e => { // --1
+    if(form.email.value !== form.email_confirm.value) { // --3
+      const contentField = document.getElementById('content_field'); // --9
+      contentField.parentNode.insertBefore(element, contentField); // --8
       emailComfirmField.classList.add("alert_bg");
     } else {
+      emailComfirmField.classList.remove("alert_bg");
       element.removeChild(message);
     }
   });
