baf0772ea336148118dface7d868694f87644341
YuukiIshibashi <robopowerful.v@gmail.com>
2021-02-02 16:50:13 +0900

    クライアントJS課題修正

--- javascripts/validation_exercise.js
+++ javascripts/validation_exercise.js
@@ -1,18 +1,21 @@
 function emailValidation() {
   const form = document.getElementById('form');
-  const emailComfirmField = document.getElementById('email_confirm'); // --2
-  const element = document.createElement('p'); // --4
-  const message = document.createTextNode("Eメールが一致しません"); // --5
-  element.appendChild(message); // --6
-  element.classList.add("alert_color"); // --7
-  emailComfirmField.addEventListener('input', e => { // --1
-    if(form.email.value !== form.email_confirm.value) { // --3
-      const contentField = document.getElementById('content_field'); // --9
-      contentField.parentNode.insertBefore(element, contentField); // --8
-      emailComfirmField.classList.add("alert_bg");
+  const emailComfirmField = document.getElementById('email_confirm');
+  const element = document.createElement('p');
+  const message = document.createTextNode("Eメールが一致しません");
+  const contentField = document.getElementById('content_field');
+  element.appendChild(message);
+  element.setAttribute('id', 'alert');
+  element.classList.add("alert_color");
+  emailComfirmField.addEventListener('input', e => {
+    if(form.email.value !== form.email_confirm.value) {
+      if (!document.getElementById('alert')) {
+        contentField.parentNode.insertBefore(element, contentField);
+        emailComfirmField.classList.add("alert_bg");
+      }
     } else {
       emailComfirmField.classList.remove("alert_bg");
-      element.removeChild(message);
+      element.parentNode.removeChild(element);
     }
   });
 };
